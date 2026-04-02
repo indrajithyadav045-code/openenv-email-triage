d52d912345b964b183a3946aeab4eb88345b0139
Indrajit Yadav <indrajithyadav045@gmail.com>
2026-04-02 09:39:07 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -29,5 +29,10 @@ def step(action: dict):
 def state():
     return env.state()
 
+
+def main():
+    uvicorn.run("server.app:app", host="0.0.0.0", port=7860)
+
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=7860)
+    main()
